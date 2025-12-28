105069258d6a619ff81c1348683d729c35ab104b
CommanderKV <kylervisser@gmail.com>
2025-12-28 01:29:49 -0500

    Enhance Course creation logic to include current user as instructor; add validation for user presence

--- src/main/java/peerport/backend/controllers/CourseController.java
+++ src/main/java/peerport/backend/controllers/CourseController.java
@@ -1,6 +1,7 @@
 package peerport.backend.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,8 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
 import peerport.backend.model.CourseModel;
+import peerport.backend.model.UserModel;
+import peerport.backend.service.AuthService;
 import peerport.backend.service.CourseService;
+import peerport.backend.service.EnrollmentService;
 
 @RestController
 @RequestMapping("/courses")
@@ -17,6 +22,9 @@ public class CourseController {
 
     @Autowired
     private CourseService courseService;
+
+    @Autowired
+    private AuthService authService;
     
     // Get all courses
     @GetMapping
@@ -37,15 +45,31 @@ public class CourseController {
     // Create new course
     @PostMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
-    public ResponseEntity<CourseModel> createCourse(@RequestBody CourseModel course) {
-        CourseModel savedCourse = courseService.saveCourse(course);
+    public ResponseEntity<CourseModel> createCourse(@Valid @RequestBody CourseModel course) {
+        // Get the current user
+        Optional<UserModel> currentUser = authService.getCurrentUser();
+
+        // Check if the user is present
+        if (!currentUser.isPresent()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        // Create the course
+        CourseModel savedCourse = courseService.createCourse(course);
+
+        // Addd the user as an instructor
+        savedCourse.addInstructor(currentUser.get());
+
+        // Save the course again
+        savedCourse = courseService.createCourse(savedCourse);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCourse);
     }
 
     // Update course
     @PutMapping("/{uuid}")
     @PreAuthorize("hasAnyRole('ADMIN', 'INSTRUCTOR')")
-    public ResponseEntity<CourseModel> updateCourse(@PathVariable String uuid, @RequestBody CourseModel course) {
+    public ResponseEntity<CourseModel> updateCourse(@PathVariable String uuid, @Valid @RequestBody CourseModel course) {
         return courseService.updateCourse(uuid, course)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
