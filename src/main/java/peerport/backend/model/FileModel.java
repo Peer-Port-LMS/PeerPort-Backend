71f48b96047b9904c8c99b6ed33c9d8144f2b353
CommanderKV <kylervisser@gmail.com>
2025-12-30 02:05:00 -0500

    Added file support. Mainly for course Image at the moment

---
+++ src/main/java/peerport/backend/model/FileModel.java
@@ -0,0 +1,103 @@
+package peerport.backend.model;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="\"Files\"")
+public class FileModel {
+    @Id
+    @Column(name="\"fileId\"")
+    @GeneratedValue(strategy=GenerationType.UUID)
+    private String fileId;
+
+    @Column(name="\"fileName\"", nullable=false, updatable=false)
+    private String fileName;
+
+    @Column(name="\"filePath\"", nullable=false, updatable=false)
+    private String filePath;
+
+    @Column(name="\"fileType\"", nullable=false)
+    private String fileType;
+
+    @CreatedDate
+    @Column(name="\"dateCreated\"")
+    private Date dateCreated;
+
+    @Column(name="\"dateUpdated\"")
+    private Date dateUpdated;
+
+    // Connections
+    @OneToOne(mappedBy="image")
+    private CourseModel course;
+
+
+    // Default constructor
+    public FileModel() { }
+
+    // Parameterized constructor
+    public FileModel(
+        String fileName,
+        String filePath,
+        String fileType
+    ) {
+        this.fileName = fileName;
+        this.filePath = filePath;
+        this.fileType = fileType;
+    }
+
+
+    // Getters and Setters
+    public String getFileId() {
+        return fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public String getURI() {
+        return "/files/" + this.fileId;
+    }
+}
---
+++ src/main/java/peerport/backend/service/FileService.java
@@ -0,0 +1,97 @@
+package peerport.backend.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import peerport.backend.database.FilesRepository;
+import peerport.backend.model.FileModel;
+
+@Service
+public class FileService {
+
+    @Autowired
+    private FilesRepository filesRepository;
+    
+    @Value("${file.upload-dir:upload-dir:uploads/}")
+    private String uploadDir;
+    private String coursesDir = uploadDir + "courses/";
+
+
+    // Regular functions
+    public Optional<FileModel> getFileById(String fileId) {
+        return filesRepository.findById(fileId);
+    }
+
+
+    // Specific functions
+
+    // Save a course image
+    public FileModel saveCourseImage(MultipartFile file, String courseId) throws IOException {
+        // Get the file extension
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+
+        // Create the new file name
+        String fileName = "course_" + courseId + (fileExtension.isEmpty() ? "" : "." + fileExtension);
+
+        // Save the image
+        String savedImagePath = saveImage(file, this.coursesDir + fileName);
+
+        // Return a new FileModel
+        FileModel newFile = new FileModel(fileName, savedImagePath, fileExtension);
+
+        // Save the file model to the database
+        return filesRepository.save(newFile);
+    }
+
+    // Delete a course image
+    public void deleteCourseImage(FileModel file) throws IOException, IllegalArgumentException {
+        // Delete the file from the repo first
+        filesRepository.delete(file);
+
+        // Delete the file from the filesystem
+        if (!deleteFile(file.getFilePath())) {
+            throw new IllegalArgumentException("File not found on the filesystem: " + file.getFilePath());
+        }
+    }
+
+    // Get file extension
+    private String getFileExtension(String fileName) {
+        if (fileName == null || !fileName.contains(".")) {
+            return "";
+        }
+        return fileName.substring(fileName.lastIndexOf('.') + 1);
+    }
+
+    // Saves an image
+    private String saveImage(MultipartFile file, String fileName) throws IOException {
+        // Create directory if it doesn't exsit
+        Path uploadPath = Paths.get(uploadDir);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        // Save the file
+        Path filePath = uploadPath.resolve(fileName);
+        Files.write(filePath, file.getBytes());
+
+        return filePath.toString();
+    }
+
+    // Delete a file
+    private boolean deleteFile(String path) throws IOException{
+        Path filePath = Paths.get(path);
+        if (Files.exists(filePath)) {
+            Files.delete(filePath);
+            return true;
+        }
+        return false;
+    }
+}
