5e4154bf54c141dbe31d1c6e0f3ebc9e9367a598
CommanderKV <kylervisser@gmail.com>
2026-01-03 18:07:56 -0500

    Refactor AssignmentService: Enhance method documentation and improve user authorization checks

--- src/main/java/peerport/backend/service/AssignmentService.java
+++ src/main/java/peerport/backend/service/AssignmentService.java
@@ -7,8 +7,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import peerport.backend.database.AssignmentsRepository;
 import peerport.backend.exceptions.assignments.AssignmentNotFoundException;
+import peerport.backend.exceptions.users.UserNotAuthenticatedException;
+import peerport.backend.exceptions.users.UserNotAuthorizedException;
 import peerport.backend.model.AssignmentModel;
 import peerport.backend.model.CourseModel;
 import peerport.backend.model.UserModel;
@@ -26,10 +29,22 @@ public class AssignmentService {
     @Autowired
     private CourseService courseService;
 
-    // Create Assignment
+    /**
+     * Create assignment
+     * 
+     * @param assignment - AssignmentModel to create
+     * @param courseId - ID of the course to create the assignment for
+     * @return The created AssignmentModel
+     * @throws CourseNotFoundException if course not found (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthorizedException if user is not authorized to edit (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthenticatedException if user is not authenticated (Handled in GlobalExceptionHandler)
+     */
     public AssignmentModel createAssignment(AssignmentModel assignment, String courseId) {
         // Get the course by ID
         CourseModel course = courseService.getCourseById(courseId);
+
+        // Check if the user is allowed to edit the course
+        courseService.userAllowedToEditCourse(course);
         
         // Set the course to the assignment
         assignment.setCourse(course);
@@ -38,8 +53,14 @@ public class AssignmentService {
         return assignmentRepository.save(assignment);
     }
 
-    // Get all assignments
-    public List<AssignmentModel> getAllAssignments() throws IllegalArgumentException {
+    /**
+     * Get all assignments
+     * 
+     * @return List of AssignmentModels
+     * @throws UserNotAuthenticatedException if user is not authenticated (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthorizedException if user is not authorized to view (Handled in GlobalExceptionHandler)
+     */
+    public List<AssignmentModel> getAllAssignments() {
         // Get the users role
         UserModel user = authService.getCurrentUser();
 
@@ -49,7 +70,7 @@ public class AssignmentService {
         }
 
         // Get all courses the user is in
-        CourseModel[] courses = courseService.getAllCourses().toArray(new CourseModel[0]);
+        List<CourseModel> courses = courseService.getAllCourses();
 
         // Get all the assignments for those courses
         List<AssignmentModel> assignments = new ArrayList<>();
@@ -61,7 +82,13 @@ public class AssignmentService {
         return assignments;
     }
 
-    // Get assignment by ID
+    /**
+     * Get assignment by ID
+     * 
+     * @param assignmentId - ID of the assignment to get
+     * @return AssignmentModel with the given ID
+     * @throws AssignmentNotFoundException if assignment not found (Handled in GlobalExceptionHandler)
+     */
     public AssignmentModel getAssignmentById(String assignmentId) {
         // Get the assignment by ID
         Optional<AssignmentModel> assignment = assignmentRepository.findById(assignmentId);
@@ -70,60 +97,94 @@ public class AssignmentService {
         if (assignment.isEmpty()) {
             throw new AssignmentNotFoundException(assignmentId);
         }
+
+        // Return the assignment
+        return assignment.get();
     }
 
-    // Update assignment
-    public Optional<AssignmentModel> updateAssignment(String assignmentId, AssignmentModel updatedAssignment) {
-        return assignmentRepository.findById(assignmentId).map(assignment -> {
-            assignment.setName(updatedAssignment.getName());
-            assignment.setDescription(updatedAssignment.getDescription());
-            assignment.setVisible(updatedAssignment.getVisible());
-            assignment.setDueDate(updatedAssignment.getDueDate());
-            assignment.setDateCreated(updatedAssignment.getDateCreated());
-            assignment.setDateUpdated(updatedAssignment.getDateUpdated());
-            assignment.setCourse(updatedAssignment.getCourse());
-            return assignmentRepository.save(assignment);
-        });
+    /**
+     * Update assignment
+     * 
+     * @param assignmentId - ID of the assignment to update
+     * @param updatedAssignment - AssignmentModel containing updated fields
+     * @return Updated AssignmentModel
+     * @throws AssignmentNotFoundException if assignment not found (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthorizedException if user is not authorized to edit (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthenticatedException if user is not authenticated (Handled in GlobalExceptionHandler)
+     */
+    @Transactional
+    public AssignmentModel updateAssignment(String assignmentId, AssignmentModel updatedAssignment) {
+        // Get the assignment
+        AssignmentModel assignment = getAssignmentById(assignmentId);
+
+        // Check if the user is authorized to edit the assignment
+        userAllowedToEditAssignment(assignment);
+
+        // Update the fields
+        assignment.setName(updatedAssignment.getName());
+        assignment.setDescription(updatedAssignment.getDescription());
+        assignment.setVisible(updatedAssignment.getVisible());
+        assignment.setDueDate(updatedAssignment.getDueDate());
+        assignment.setDateUpdated(updatedAssignment.getDateUpdated());
+
+        // Return the assignment
+        return assignmentRepository.save(assignment);
     }
 
-    // Patch / partially update assignment
-    public AssignmentModel patchAssignment(String assignmentId, AssignmentModel updatedFields) throws IllegalArgumentException {
+    /**
+     * Patch assignment
+     * 
+     * @param assignmentId - ID of the assignment to patch
+     * @param updatedFields - AssignmentModel containing fields to update
+     * @return Updated AssignmentModel
+     * @throws AssignmentNotFoundException if assignment not found (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthorizedException if user is not authorized to edit (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthenticatedException if user is not authenticated (Handled in GlobalExceptionHandler)
+     */
+    @Transactional
+    public AssignmentModel patchAssignment(String assignmentId, AssignmentModel updatedFields) {
         // Get the existing assignment
-        Optional<AssignmentModel> assignment = assignmentRepository.findById(assignmentId);
+        AssignmentModel assignment = getAssignmentById(assignmentId);
 
-        // If assignment doesn't exist return empty
-        if (assignment.isEmpty()) {
-            throw new IllegalArgumentException("Assignment with ID " + assignmentId + " not found");
-        }
+        // Check if the user is authorized to edit the assignment
+        userAllowedToEditAssignment(assignment);
 
         // If assignment exists, update only the provided fields
-        AssignmentModel existingAssignment = assignment.get();
         if (updatedFields.getName() != null) {
-            existingAssignment.setName(updatedFields.getName());
+            assignment.setName(updatedFields.getName());
         }
         if (updatedFields.getDescription() != null) {
-            existingAssignment.setDescription(updatedFields.getDescription());
+            assignment.setDescription(updatedFields.getDescription());
         }
         if (updatedFields.getVisible() != null) {
-            existingAssignment.setVisible(updatedFields.getVisible());
+            assignment.setVisible(updatedFields.getVisible());
         }
         if (updatedFields.getDueDate() != null) {
-            existingAssignment.setDueDate(updatedFields.getDueDate());
+            assignment.setDueDate(updatedFields.getDueDate());
         }
         if (updatedFields.getDateUpdated() != null) {
-            existingAssignment.setDateUpdated(updatedFields.getDateUpdated());
+            assignment.setDateUpdated(updatedFields.getDateUpdated());
         }
         if (updatedFields.getCourse() != null) {
-            existingAssignment.setCourse(updatedFields.getCourse());
+            assignment.setCourse(updatedFields.getCourse());
         }
 
         // Save and return the updated assignment
-        return assignmentRepository.save(existingAssignment);
+        return assignmentRepository.save(assignment);
     }
 
-    // Delete assignment
+    /**
+     * Delete assignment
+     * 
+     * @param assignmentId - ID of the assignment to delete
+     * @throws AssignmentNotFoundException if assignment not found (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthorizedException if user is not authorized to delete (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthenticatedException if user is not authenticated (Handled in GlobalExceptionHandler)
+     */
+    @Transactional
     public void deleteAssignment(String assignmentId) {
         // Check if the user is authorized to delete the assignment
+        userAllowedToEditAssignment(assignmentId);
 
         // Delete the assignment by ID
         assignmentRepository.deleteById(assignmentId);
@@ -131,11 +192,26 @@ public class AssignmentService {
 
 
     // Helpers //
+    /**
+     * Check if user is allowed to edit the assignment
+     * 
+     * @param assignment - AssignmentModel to check
+     * @throws UserNotAuthorizedException if user is not authorized to edit (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthenticatedException if user is not authenticated (Handled in GlobalExceptionHandler)
+     */
     private void userAllowedToEditAssignment(AssignmentModel assignment) {
         // Check if user is allowed to edit the assignment
         courseService.userAllowedToEditCourse(assignment.getCourse());
     }
 
+    /**
+     * Check if user is allowed to edit the assignment by assignment ID
+     * 
+     * @param assignmentId - ID of the AssignmentModel to check
+     * @throws AssignmentNotFoundException if assignment not found (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthorizedException if user is not authorized to edit (Handled in GlobalExceptionHandler)
+     * @throws UserNotAuthenticatedException if user is not authenticated (Handled in GlobalExceptionHandler)
+     */
     private void userAllowedToEditAssignment(String assignmentId) {
         // Get the assignment by ID
         AssignmentModel assignment = getAssignmentById(assignmentId);
