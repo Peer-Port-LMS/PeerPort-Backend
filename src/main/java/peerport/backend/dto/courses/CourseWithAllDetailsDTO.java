aaf9250566f31ae787fc66e0575a23b0ae47e16a
CommanderKV <kylervisser@gmail.com>
2026-03-01 17:04:02 -0500

    Add method to filter visible assignments in CourseWithAllDetailsDTO

--- src/main/java/peerport/backend/dto/courses/CourseWithAllDetailsDTO.java
+++ src/main/java/peerport/backend/dto/courses/CourseWithAllDetailsDTO.java
@@ -1,5 +1,8 @@
 package peerport.backend.dto.courses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import peerport.backend.dto.UserDTO;
 import peerport.backend.dto.announcements.AnnouncementDTO;
 import peerport.backend.dto.assignments.AssignmentDTO;
@@ -9,4 +12,13 @@ public class CourseWithAllDetailsDTO extends CourseDTO {
     public UserDTO[] instructors;
     public AnnouncementDTO[] announcements;
     public AssignmentDTO[] assignments;
+
+    public void removePrivilegedDetails() {
+        List<AssignmentDTO> visibleAssignments = new ArrayList<>();
+        for (AssignmentDTO assignment : assignments) {
+            if (assignment.visible == true) {
+                visibleAssignments.add(assignment);
+            }
+        }
+    }
 }
