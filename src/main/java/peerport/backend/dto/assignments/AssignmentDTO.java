0a5f5e1bdf4a88e077e47e2f6787508188f5a447
CommanderKV <kylervisser@gmail.com>
2026-03-01 17:04:07 -0500

    Add gradesVisible field to AssignmentDTO for enhanced grade visibility management

--- src/main/java/peerport/backend/dto/assignments/AssignmentDTO.java
+++ src/main/java/peerport/backend/dto/assignments/AssignmentDTO.java
@@ -12,7 +12,9 @@ public class AssignmentDTO {
     public String description;
     public Date dueDate;
     public Boolean visible;
+    public Boolean gradesVisible;
     public Date dateCreated;
     public Date dateUpdated;
     public List<FileDTO> files;
+    public float grade; // Students: Their grade, Instructors: Average grade for the assignment
 }
