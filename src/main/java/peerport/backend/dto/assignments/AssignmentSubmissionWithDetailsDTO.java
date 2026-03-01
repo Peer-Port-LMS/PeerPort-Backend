4383c8d67169bfdd2ed5f7d602346bc194d26304
CommanderKV <kylervisser@gmail.com>
2026-03-01 15:47:14 -0500

    Add Optional grade field to AssignmentSubmissionWithDetailsDTO

--- src/main/java/peerport/backend/dto/assignments/AssignmentSubmissionWithDetailsDTO.java
+++ src/main/java/peerport/backend/dto/assignments/AssignmentSubmissionWithDetailsDTO.java
@@ -1,12 +1,15 @@
 package peerport.backend.dto.assignments;
 
 import java.util.List;
+import java.util.Optional;
 
 import peerport.backend.dto.FileDTO;
+import peerport.backend.dto.GradeDTO;
 import peerport.backend.dto.UserDTO;
 
 public class AssignmentSubmissionWithDetailsDTO extends AssignmentSubmissionDTO {
     public UserDTO user;
     public AssignmentDTO assignment;
+    public Optional<GradeDTO> grade;
     public List<FileDTO> submittedFiles;
 }
